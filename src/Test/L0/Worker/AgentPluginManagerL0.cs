c1a3840fb247ff71bb440e6cbe26fcee364320f0
Tommy Petty <jopetty@microsoft.comwq>
2019-12-31 14:54:10 -0500

    Added more extensive testing

--- src/Test/L0/Worker/AgentPluginManagerL0.cs
+++ src/Test/L0/Worker/AgentPluginManagerL0.cs
@@ -5,11 +5,30 @@ using Xunit;
 using Pipelines = Microsoft.TeamFoundation.DistributedTask.Pipelines;
 using Agent.Plugins.PipelineArtifact;
 using Agent.Plugins.PipelineCache;
+using System.Collections.Generic;
+
 
 namespace Microsoft.VisualStudio.Services.Agent.Tests.Worker
 {
     public sealed class AgentPluginManagerL0
     {
+
+        private class AgentPluginTaskTest
+        {
+            public string Name;
+            public Guid TaskGuid;
+            public List<string> ExpectedTaskPlugins;
+
+            public void RunTest(AgentPluginManager manager)
+            {
+                var taskPlugins = manager.GetTaskPlugins(TaskGuid);
+                Assert.True(taskPlugins.Count == ExpectedTaskPlugins.Count, $"{Name} has {ExpectedTaskPlugins.Count} Task Plugin(s)");
+                foreach (var s in ExpectedTaskPlugins)
+                {
+                    Assert.True(taskPlugins.Contains(s), $"{Name} contains '{s}'");
+                }
+            }
+        }
 
         [Fact]
         [Trait("Level", "L0")]
@@ -22,10 +41,62 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.Worker
                 var agentPluginManager = new AgentPluginManager();
                 agentPluginManager.Initialize(tc);
 
-                Assert.True(agentPluginManager.GetTaskPlugins(Pipelines.PipelineConstants.CheckoutTask.Id).Count == 2, "Checkout Task has 2 Task Plugins");
-                Assert.True(agentPluginManager.GetTaskPlugins(PipelineArtifactPluginConstants.DownloadPipelineArtifactTaskId).Count == 7, "Download Pipline Artifact Task has 2 Task Plugins");
-                Assert.True(agentPluginManager.GetTaskPlugins(PipelineArtifactPluginConstants.PublishPipelineArtifactTaskId).Count == 3, "Publish Pipeline Artifact Task has 2 Task Plugins");
-                Assert.True(agentPluginManager.GetTaskPlugins(PipelineCachePluginConstants.CacheTaskId).Count == 2, "Pipeline Cache Task has 2 Task Plugins");
+                List<AgentPluginTaskTest> tests = new List<AgentPluginTaskTest>
+                {
+                    new AgentPluginTaskTest()
+                    {
+                        Name = "Checkout Task",
+                        TaskGuid = Pipelines.PipelineConstants.CheckoutTask.Id,
+                        ExpectedTaskPlugins = new List<string>
+                        {
+                            "Agent.Plugins.Repository.CheckoutTask, Agent.Plugins",
+                            "Agent.Plugins.Repository.CleanupTask, Agent.Plugins",
+                        }
+                    },
+                    new AgentPluginTaskTest()
+                    {
+                        Name = "Download Pipline Artifact Task",
+                        TaskGuid = PipelineArtifactPluginConstants.DownloadPipelineArtifactTaskId,
+                        ExpectedTaskPlugins = new List<string>
+                        {
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTask, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV1, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV1_1_0, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV1_1_1, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV1_1_2, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV1_1_3, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.DownloadPipelineArtifactTaskV2_0_0, Agent.Plugins",
+                        }
+                    },
+                    new AgentPluginTaskTest()
+                    {
+                        Name = "Publish Pipeline Artifact Task",
+                        TaskGuid = PipelineArtifactPluginConstants.PublishPipelineArtifactTaskId,
+                        ExpectedTaskPlugins = new List<string>
+                        {
+                            "Agent.Plugins.PipelineArtifact.PublishPipelineArtifactTask, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.PublishPipelineArtifactTaskV1, Agent.Plugins",
+                            "Agent.Plugins.PipelineArtifact.PublishPipelineArtifactTaskV0_140_0, Agent.Plugins"
+                        }
+                    },
+                    new AgentPluginTaskTest()
+                    {
+                        Name = "Pipeline Cache Task",
+                        TaskGuid = PipelineCachePluginConstants.CacheTaskId,
+                        ExpectedTaskPlugins = new List<string>
+                        {
+                            "Agent.Plugins.PipelineCache.SavePipelineCacheV0, Agent.Plugins",
+                            "Agent.Plugins.PipelineCache.RestorePipelineCacheV0, Agent.Plugins",
+                        }
+                    },
+                };
+
+                foreach (var test in tests)
+                {
+                    test.RunTest(agentPluginManager);
+                }
+
+
             }
         }
 
